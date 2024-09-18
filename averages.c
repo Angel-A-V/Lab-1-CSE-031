6d1a42acdedcf18cbe975009e156acb0accdc124
Angel Vargas <aaawwwzzzeee123@gmail.com>
2024-09-17 21:45:33 -0700

    finished

--- averages.c
+++ averages.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 
-int main() {
-    int num, sumEven = 0, sumOdd = 0, countEven = 0, countOdd = 0, firstInput = 1;
-    float avgEven = 0.0, avgOdd = 0.0;
-    int count = 1;
+//Function for the suffix changes
+const char* thesuffix(int count){
+    if(count % 10 == 1 && count % 100 != 11){
+        return "st";
+    } else if(count % 10 == 2){
+        return "nd";
+    } else if(count % 10 == 3){
+        return "rd";
+    } else {
+        return "th";
+    }
+}
 
-    while (1) {
-        printf("Enter the %d%s value: ", count,
-            (count % 10 == 1 && count % 100 != 11) ? "st" :
-            (count % 10 == 2 && count % 100 != 12) ? "nd" :
-            (count % 10 == 3 && count % 100 != 13) ? "rd" : "th");
+int main(){
 
+    int count = 1, evencount = 0, oddcount = 0, num, firstnum = 1, sumEven = 0.0, sumOdd = 0.0;
+    float avgeven = 0.0, avgodd = 0.0;
+    
+    while(1){
+
+        printf("Enter the %d%s value: ", count, thesuffix(count));
         scanf("%d", &num);
 
-        if (num == 0) {
-            if (firstInput) {
+        //if num equal zero end the program test case senario
+        if(num == 0){
+            if(firstnum){
                 printf("\nThere is no average to compute.");
             }
             break;
         }
-        firstInput = 0;
+        firstnum = 0;
 
         // Calculate sum of digits
         int temp = num, digitSum = 0;
@@ -31,28 +42,30 @@ int main() {
         // Check if the sum of digits is even or odd
         if (digitSum % 2 == 0) {
             sumEven += num;
-            countEven++;
+            evencount++;
         } else {
             sumOdd += num;
-            countOdd++;
+            oddcount++;
         }
-
         count++;
     }
 
-    // Calculate averages
-    if (countEven > 0) {
-        avgEven = (float)sumEven / countEven;
+    if (evencount > 0) {
+        avgeven = (float)sumEven / evencount;
     }
 
-    if (countOdd > 0) {
-        avgOdd = (float)sumOdd / countOdd;
+    if (oddcount > 0) {
+        avgodd = (float)sumOdd / oddcount;
     }
 
-    // Print results with two decimal places
-    if (!firstInput) { // Only print if there are valid inputs
-        printf("\nAverage of input values whose digits sum up to an even number: %.2f\n", avgEven);
-        printf("Average of input values whose digits sum up to an odd number:  %.2f\n", avgOdd);
+    printf("\n");
+
+    if (!firstnum && evencount > 0) { // Only print if there are valid inputs
+        printf("Average of input values whose digits sum up to an even number: %.2f\n", avgeven);
+    }
+
+    if (!firstnum && oddcount > 0) { // Only print if there are valid inputs
+        printf("Average of input values whose digits sum up to an odd number:  %.2f\n", avgodd);
     }
 
     return 0;
